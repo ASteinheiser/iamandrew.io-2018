7ae63c7348ac23cd64d5d9a41a36f089611c6e28
Andrew Steinheiser <me@iamandrew.io>
2018-04-14 23:48:08 -0700

    fixed up navigation

--- src/components/navigation/top-nav/top-nav.js
+++ src/components/navigation/top-nav/top-nav.js
@@ -7,15 +7,15 @@ export default class TopNav extends React.Component {
     return (
       <div className='top-nav-bar'>
         <div className='space-between max-width'>
-          <div>
+          <div onClick={() => this.props.history.push('/')} className='top-nav-iamandrew-logo'>
             {'iamandrew'}
           </div>
           <div className='top-nav-links-container'>
+            <div onClick={() => this.props.history.push('/projects')} className='top-nav-item margin-right'>
+              {'projects'}
+            </div>
             <div onClick={() => this.props.history.push('/about')} className='top-nav-item'>
               {'about'}
-            </div>
-            <div onClick={() => this.props.history.push('/projects')} className='top-nav-item'>
-              {'projects'}
             </div>
           </div>
         </div>
