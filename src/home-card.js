236ea0bfae4dd165b59bffc82d363073d74fc29a
Andrew Steinheiser <me@iamandrew.io>
2018-04-14 19:53:34 -0700

    got the star demo working

---
+++ src/home-card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './index.css';
+
+export default class Router extends React.Component {
+  render() {
+    return (
+      <div className='home-card-container'>
+        <span>
+          iamandrew
+        </span>
+        <span>
+          > projects
+        </span>
+        <span>
+          > about
+        </span>
+      </div>
+    )
+  }
+}
