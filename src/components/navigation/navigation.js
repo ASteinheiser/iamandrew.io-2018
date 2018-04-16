b8c22eae7a1b4bf03a1bb1627e74ecf86c7b9f10
Andrew Steinheiser <me@iamandrew.io>
2018-04-15 17:44:27 -0700

    removed footer cus fok that

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import TopNav from './top-nav';
-import Footer from './footer';
 import './index.css';
 
 export default class Navigation extends Component {
@@ -13,8 +12,6 @@ export default class Navigation extends Component {
         <div className='navigation-content max-width'>
           {this.props.children}
         </div>
-
-        <Footer />
       </div>
     )
   }
