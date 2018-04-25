bd376315526fdaa66eb112eb522531f3861ee797
Andrew Steinheiser <me@iamandrew.io>
2018-04-24 19:48:07 -0700

    unregister service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-import Router                from './router';
-import registerServiceWorker from './registerServiceWorker';
+import Router         from './router';
+import { unregister } from './registerServiceWorker';
 
 import './index.css';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
